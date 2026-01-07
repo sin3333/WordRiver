04a0aa89ee7646312bff2e4dd799f87c895a2a55
sin3333 <shinnosuke.20041@gmail.com>
2026-01-07 11:23:24 +0900

    feat: 単語追加、ストレージ保存の処理基盤を追加（中身はこれから）

--- hooks/useWordStorage.tsx
+++ hooks/useWordStorage.tsx
@@ -1,6 +1,11 @@
 import { useState, React } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-funvtion useWordStorage() {
-    const [WordList, setWordList] = useState<Array[]>;
-    // TODO
+export funvtion usewordStorage() {
+
+    return {
+        load: async (): Promise<word[]> => [],
+        save: async(word word[]) => { },
+};
+
 }
--- hooks/useWords.tsx
+++ hooks/useWords.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { useState, React, use } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { usewordStorage } from "./useWordStorage";
+import { WordItem } from "../types/word";
 
 function useWord() {
-    // TODO
+    const storage = usewordStorage();
+    const [wordList, setWordList] = useState<WordItem[]>([]);
+
+    useEffect(() => {
+        storage.load().then(setWordList);
+    }, []);
+
+    const addWord = async (WordItem: WordItem) => {
+        const newWordList = [...wordList, WordItem];
+        setWordList(newWordList);
+        storage.save(newWordList);
+    };
+
+    return {
+        wordList,
+        addWord,
+    }
 }

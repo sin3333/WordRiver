994265e357ba840847bb052a6021e53c7aa5c9a3
sin3333 <shinnosuke.20041@gmail.com>
2026-01-08 09:13:09 +0900

    feat: WordStorageでーた読み込み処理

--- hooks/useWordStorage.tsx
+++ hooks/useWordStorage.tsx
@@ -1,11 +1,24 @@
 import { useState, React } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export funvtion usewordStorage() {
+import { WordItem } from "../types/word";
+import { WordStorage } from "@/storage/WordStorage";
+
+export function useWordStorage() {
 
     return {
-        load: async (): Promise<word[]> => [],
-        save: async(word word[]) => { },
-};
+        load: async (): Promise<WordItem[]> => {
+            try {
+                const value = await AsyncStorage.getItem('WordStorage');
+                if (value !== null) {
+                    return JSON.parse(value) as WordItem[];
+                }
+                return [];
+            } catch (e) {
+                console.log('読み込みエラー:', e);
+            }
+        },
+        save: async (WordItem: WordItem[]) => { },
+    };
 
 }

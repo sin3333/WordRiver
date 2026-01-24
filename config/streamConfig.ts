fd6039eed3f7f76fe41cdb235af9c489a4ae1b44
sin3333 <shinnosuke.20041@gmail.com>
2026-01-24 10:39:50 +0900

    feat: play画面機能/型定義、コンフィグデフォルト設計

---
+++ config/streamConfig.ts
@@ -0,0 +1,9 @@
+import type { StreamConfig } from "./types/streamItemTypes";
+
+export const Default_stream_config: StreamConfig = {
+    baseDurationMs: 4500,
+    fromOffsetY: 40,
+    toOffsetY: 60,
+    lanePaddingLeft: 16,
+    MaxWidthRatio: 0.8,
+};
---
+++ types/streamItemTypes.ts
@@ -0,0 +1,16 @@
+export type StreamItem = {
+    id: string;
+    word: string;
+
+    laneIndex: number;
+    createdAt: number;
+    durationMs: number;
+}
+
+export type StreamConfig = {
+    baseDurationMs: number;
+    fromOffsetY: number; //画面下スタート位置
+    toOffsetY: number; //画面上終了位置
+    lanePaddingLeft: number;
+    MaxWidthRatio: number; //画面の最大幅割合。文字の最大幅
+};

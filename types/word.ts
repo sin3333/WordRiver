648437e2e9098239d6a9eee5ea4d722423571cb6
sin3333 <shinnosuke.20041@gmail.com>
2026-02-12 09:21:10 +0900

    WIP:単語データをフォルダ分け

--- types/word.ts
+++ types/word.ts
@@ -1,6 +1,19 @@
+export type Folder = {
+    id: string;
+    name: string;
+    createdAt: number;
+};
+
 export type WordItem = {
     id: string;
     word: string;
     note?: string;
     createdAt: number; // ISO
+    folderId: string;
 };
+
+export type WordsStore = {
+    folders: Folder[];
+    words: WordItem[];
+    activeFolderId: string;
+}
